69d8356f3d5cbd24d5b860b3bd06570a8e44cd3a
Matthijs Boet <m.boet@chello.nl>
2020-04-07 13:51:49 +0200

    Updates

---
+++ precache-manifest.2479ce9c89dfd0ca82bd398b3a22ae04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c5d6f5a3860493316fc5705ff1310e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0c3fbe748be92ce361af",
+    "url": "/monster-rolodex/static/css/main.09636c6e.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c3fbe748be92ce361af",
+    "url": "/monster-rolodex/static/js/main.7e61c7c7.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
